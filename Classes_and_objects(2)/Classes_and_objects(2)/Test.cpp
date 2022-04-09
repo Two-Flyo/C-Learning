c335042e08095bb21b6ddcac4a27482801292d5e
lrf <465197183@qq.com>
2022-04-09 23:44:52 +0800

    C++类和对象完善

--- Classes_and_objects(2)/Classes_and_objects(2)/Test.cpp
+++ Classes_and_objects(2)/Classes_and_objects(2)/Test.cpp
@@ -1,59 +1,188 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include<Windows.h>
 
-//using namespace std;
+using namespace std;
 using std::endl;
 using std::cout;
 
-class A
-{
-public:
-	A(int a = 0)
-		:_a(a)
-	{
-		_sCount++;
-	}
-	A(const A& aa)
-		:_a(aa._a)
-	{
-		_sCount++;
-	}
-	//static修饰的函数，没有this指针，只能访问静态的成员变量
-	static int GetCount()
-	{
-		return _sCount;
-	}
-private:
-	int _a;
-	//静态成员变量属于整个类，所有对象，生命周期在整个程序运行期间
-	//在类的成员函数中，可以随便访问
-	static int _sCount;
+class A {
+
+	class B {
+
+	};
 };
 
-int A::_sCount = 0; //定义初始化
+//class Date
+//{
+//public:
+//	Date(int year=0, int mouth=1, int day=1)
+//	{
+//		_year = year;
+//		_mouth = mouth;
+//		_day = day;
+//	}
+//	void Print()
+//	{
+//		//cout << _year << '-' << _mouth << '-' << _day << endl;
+//		printf("%04d-%02d-%02d\n", _year, _mouth, _day);
+//	}
+//
+//private:
+//	int _year;
+//	int _mouth;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1;
+//	Date d2(2002, 03, 30);
+//	d1.Print();
+//	d2.Print();
+//	return 0;
+//}
 
-void f(A a)
-{
+//class Date; // 前置声明
+//class Time
+//{
+//	friend class Date; // 声明日期类为时间类的友元类，则在日期类中就直接访问Time类员变量
+//public:
+//	Time(int hour=0, int minute=0, int second=0)
+//		: _hour(hour)
+//		, _minute(minute)
+//		, _second(second)
+//	{}
+//private:
+//	int _hour;
+//	int _minute;
+//	int _second;
+//};
+//class Date
+//{
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{
+//		_t._hour = 0;
+//		_t._minute = 0;
+//		_t._second = 0;
+//	}
+//	void SetTimeOfDate(int hour, int minute, int second)
+//	{
+//		// 直接访问时间类私有的成员变量
+//		_t._hour = hour;
+//		_t._minute = minute;
+//		_t._second = second;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//	Time _t;
+//};
+//
+//int main()
+//{
+//
+//	return 0;
+//}
 
-}
 
-int main()
-{
-	A a1;
-	A a2 = 1;
-	A a3(a2);
-	f(a3);
-	//_sCount 公有
-	//cout << A::_sCount << endl;
-	//cout << a2._sCount << endl;
-	
-	// _sCount 私有
-	cout << a2.GetCount() << endl;//写一个Get函数
-	cout << A::GetCount() << endl;//static修饰函数，则可以通过类去访问
+//class Time
+//{
+//	friend class Date;
+//};
 
+//class B
+//{
+//public:
+//	B(int b = 0)
+//		:_b(b)
+//	{}
+//private:
+//	int _b;
+//};
+//
+////C++ 11  
+//class A
+//{
+//public:
+//	//初始化列表阶段，没有对成员变量初始化，这里就会使用缺省值初始化
+//	A(int a1)
+//	{}
+//private:
+//	int _a1 = 0;//给成员变量缺省值
+//	B _bb=10;
+//	B _bb2 = B(20);
+//	int* p = (int*)malloc(40);
+//	int arr[10] = { 1,2,3,4,5 };
+//	//静态的不能这样给缺省值，必须在类外面全局定义初始化
+//	//static int _sCount = 0;
+//
+//};
+//
+//int main()
+//{
+//	A aa(1);
+//	return 0;
+//}
 
-	return 0;
-}
+//class A
+//{
+//public:
+//	A(int a = 0)
+//		:_a(a)
+//	{
+//		_sCount++;
+//	}
+//	A(const A& aa)
+//		:_a(aa._a)
+//	{
+//		_sCount++;
+//	}
+//	int test()
+//	{
+//		GetCount();
+//	}
+//	//static修饰的函数，没有this指针，只能访问静态的成员变量
+//	static int GetCount()
+//	{
+//		test();//error
+//		return _sCount;
+//	}
+//private:
+//	int _a;
+//	//静态成员变量属于整个类，所有对象，生命周期在整个程序运行期间
+//	//在类的成员函数中，可以随便访问
+//	static int _sCount;
+//};
+//
+//int A::_sCount = 0; //定义初始化
+//
+//void f(A a)
+//{
+//
+//}
+//
+//int main()
+//{
+//	A a1;
+//	A a2 = 1;
+//	A a3(a2);
+//	f(a3);
+//	//_sCount 公有
+//	//cout << A::_sCount << endl;
+//	//cout << a2._sCount << endl;
+//	
+//	// _sCount 私有
+//	cout << a2.GetCount() << endl;//写一个Get函数
+//	cout << A::GetCount() << endl;//static修饰函数，则可以通过类去访问
+//
+//
+//	return 0;
+//}
 
 //class Date
 //{
