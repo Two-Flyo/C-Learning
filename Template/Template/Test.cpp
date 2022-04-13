6901bc1dec50345a2c2d92c438e896df600462de
lrf <465197183@qq.com>
2022-04-13 23:57:36 +0800

    类模板初步

--- Template/Template/Test.cpp
+++ Template/Template/Test.cpp
@@ -3,26 +3,49 @@
 
 using namespace std;
 
-//函数模板
-
 template<class T>
-
-T Add(const T& x1, const T& x2)
+class A
 {
-	return x1 + x2;
-}
+public:
+	A(T a,T b)
+	:_a(a),
+	_b(b)
+	{
+
+	}
+
+private:
+	T _a;
+	T _b; 
+};
 
 int main()
 {
-	int a = 2;
-	double b = 2.333;
-	//cout<<Add(a, b)<<endl; error
-	cout << Add((double)a, b) << endl;
-
-	cout << Add<int>(a, b) << endl;//指定T使用int
-	cout << Add<double>(a, b) << endl;//指定T使用double
+	A<int> a1(1, 3);
+	A<double> a2(1, 3);
 	return 0;
 }
+
+
+////函数模板
+//template<class T>
+//T Add(const T& x1, const T& x2)
+//{
+//	return x1 + x2;
+//}
+//
+//int main()
+//{
+//	int a = 2;
+//	double b = 2.333;
+//	//cout<<Add(a, b)<<endl; error
+//	cout << Add((double)a, b) << endl;
+//	
+//	//显式实例化
+//	cout << Add<int>(a, b) << endl;//指定T使用int
+//	cout << Add<double>(a, b) << endl;//指定T使用double
+//	return 0;
+//}
 
 //template<class T>                //template<typename T> //模板参数列表 --- 参数类型
 //void Swap(T& x1, T& x2)           //函数参数列表  -- 参数类型
