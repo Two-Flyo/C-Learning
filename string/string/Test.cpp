4503b33bfa7ad8cf2b879110c72d65e19ce519d6
lrf <465197183@qq.com>
2022-04-17 23:10:50 +0800

    string迭代器

--- string/string/Test.cpp
+++ string/string/Test.cpp
@@ -3,29 +3,135 @@
 #include<string>
 using namespace std;
 
-template<class T>
-class basic_string
+void func(const string& s)
 {
-private:
-	T* _str;
-	//···
-};
+	string::const_reverse_iterator rit = s.rbegin();
+	while (rit != s.rend())
+	{
+		cout << *rit << ' ';
+		++rit;
+	}
+	cout << endl;
+}
 
-int main()
+void Test_string()
 {
-	string s1;
-	cin >> s1;
-	string s2("hello world!");
-	string s3(s2);
-	cout << s1 << endl;
-	cout << s2 << endl;
-	cout << s3 << endl;
-	string s4(s2, 1, 4);
-	string s5("hello world!", 5);//针对的是字符数组，去字符数组的前n个字符
-	cout << s4<<endl;
-	cout << s5 << endl;
+	string s1("hello world!");
+	string::reverse_iterator rit = s1.rbegin();
+	while (rit != s1.rend())
+	{
+		cout << *rit << ' ';
+		++rit;
+	}
+
+	const string str("hello world");
+	func(str);
+	//遍历+修改
+	//(1)下标+[]
+	//for (size_t i = 0; i < s1.size(); i++)
+	//{
+	//	s1[i] += 1;
+	//}
+	//for (size_t i=0;i<s1.size();i++)
+	//{
+	//	cout << s1[i] << " ";
+	//}
+	//(2)迭代器
+	//string::iterator it = s1.begin();
+	//while (it != s1.end())
+	//{
+	//	cout << *it << " ";
+	//	++it;
+	//}
+	//cout << endl;
+	//(3)范围for
+	//for (auto& e : s1)
+	//{
+	//	e++;
+	//	cout << e << ' ';
+	//}
+	//cout << endl;
+	//for (auto e : s1)
+	//{
+	//	cout << e << ' ';
+	//}
+}
+
+int main() 
+{	
+	Test_string();
+
 	return 0;
 }
+
+
+//int main()
+//{
+//	string s1;
+//	s1.push_back('a');
+//	s1.append("bcdef");
+//	cout << s1 << endl;
+//	s1 += ' ';
+//	s1 += "hijklmn";
+//	cout << s1 << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	string s1("hello~world!");
+//	for (size_t i = 0; i < s1.size(); i++)
+//	{
+//		cout << s1.operator[](i);
+//		cout << s1[i];
+//	}
+//	for (size_t i = 0; i < s1.size(); i++)
+//	{
+//		s1[i] += 1;
+//	}
+//	cout << s1 << endl;
+//
+//	for (size_t i = 0; i < s1.size(); i++)
+//	{
+//		s1.at(i) -= 1;
+//	}
+//	cout << s1 << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	string s1;
+//	cin >> s1;
+//	cout << s1.size() << endl;
+//	cout << s1.length() << endl;
+//	cout << s1.capacity() << endl;
+//	return 0;
+//}
+
+//template<class T>
+//class basic_string
+//{
+//private:
+//	T* _str;
+//	//···
+//};
+//
+//int main()
+//{
+//	string s1;
+//	cin >> s1;
+//	string s2("hello world!");
+//	string s3(s2);
+//	cout << s1 << endl;
+//	cout << s2 << endl;
+//	cout << s3 << endl;
+//	string s4(s2, 1, 4);
+//	string s5("hello world!", 5);//针对的是字符数组，去字符数组的前n个字符
+//	cout << s4<<endl;
+//	cout << s5 << endl;
+//	return 0;
+//}
 
 
 //编码 -- 值 --符号建立映射关系 -- 编码表
