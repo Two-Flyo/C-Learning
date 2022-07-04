cf66ffd73f47fbd8670f85deebdf5be2941c7de2
lrf <465197183@qq.com>
2022-07-04 23:16:56 +0800

    vector学习

---
+++ Vector/Vector/Test.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+//void test_vector1()
+//{
+//	vector<int> v1;
+//	vector<int> v2(10, 1);
+//	vector<int> v3(v2.begin(), v2.end());
+//	vector<int> v4(v3);
+//
+//	string s("hello,world!");
+//
+//	for (int i = 0; i < v2.size(); i++)
+//	{
+//		cout << v2[i] << ' ';
+//	}
+//	cout << endl;
+//	vector<int>::iterator it = v2.begin();
+//	while (it!=v2.end())
+//	{
+//		*it -= 1;
+//		cout << *it << " ";
+//		it++;
+//	}
+//	vector<char> v5(s.begin(), s.end());
+//}
+void test_vector()
+{
+
+}
+//void test_vector2()
+//{
+//
+//	vector<int> v1;
+//	v1.resize(100);
+//	int i = 1;
+//	for (auto& x : v1)
+//	{
+//		x = i++;
+//	}
+//	for (auto x : v1)
+//	{
+//		cout << x << ' ';
+//	}
+//	auto it = find(v1.begin(), v1.end(), 76);
+//	cout << *it << endl;
+//}
+
+int main()
+{
+
+	//test_vector1();
+	test_vector();
+	return 0;
+}
