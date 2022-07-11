79479907d8f357e3abacb215a2e75c6aa8f13fd4
lrf <465197183@qq.com>
2022-07-11 21:47:21 +0800

    vector模拟实现

--- Analog_vector/Analog_vector/Test.cpp
+++ Analog_vector/Analog_vector/Test.cpp
@@ -1,16 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"vector.h"
 #include<string>
+#include<vector>
+#include<algorithm>
+#include<list>
+
 using namespace std;
+
 int main()
  {
-	//test_vector();
-	std::string ret = "1100";
-	int sum = 0;
-	for (int i = 0; i < ret.size(); i++)
-	{
-		sum = 10 * sum + stoi(""+ret[i]);
-	}
-	std::cout << sum;
+	test_vector();
 	return 0;
 }
--- Analog_vector/Analog_vector/vector.h
+++ Analog_vector/Analog_vector/vector.h
@@ -1,7 +1,7 @@
 #pragma once
 #include<iostream>
 #include<cassert>
-
+using namespace std;
 
 namespace lrf
 {
@@ -17,6 +17,48 @@ namespace lrf
 			 _finish(nullptr),
 			 _endofstorage(nullptr)
 		{}
+		template <class InputIterator>
+		vector(InputIterator first, InputIterator last)
+			:_start(nullptr),
+			 _finish(nullptr),
+			 _endofstorage(nullptr)
+		{
+			while (first != last)
+			{
+				push_back(*first);
+				++first;
+			}
+		}
+
+
+		void swap(vector<T>& v)
+		{
+			std::swap(_start, v._start);
+			std::swap(_finish, v._finish);
+			std::swap(_endofstorage, v._endofstorage);
+		}
+
+		vector(const vector<T>& v)
+		{
+			vector<T> tmp(v.begin(),v.end());
+			swap(tmp);
+		}
+
+		~vector()
+		{
+			if (_start != nullptr)
+			{
+				delete[] _start;
+				_start = _finish = _endofstorage = nullptr;
+			}
+		}
+
+		vector<T>& operator=(vector<T> v)
+		{
+			swap(v);
+
+			return *this;
+		}
 
 		iterator begin()
 		{
@@ -76,8 +118,45 @@ namespace lrf
 		}
 
 
-		void insert(iterator pos, const T& x);
-		void erase(iterator pos);
+		void pop_back()
+		{
+			assert(_finish > _start);
+			--_finish;
+		}
+
+		iterator insert(iterator pos, const T& x)  //通过设置insert的返回值类型，一定程度上“解决”迭代器失效问题
+		{
+			assert(pos >= _start && pos <= _finish);  //=_finish相当于尾插
+			if (_finish == _endofstorage)
+			{
+				//扩容之后 _start和_finish发生变化，会导致迭代器失效
+				size_t len = pos - _start;
+				reserve(capacity() == 0 ?4:capacity() * 2);
+				pos = _start + len;
+			}
+			iterator end = _finish - 1;
+			while (end >= pos)
+			{
+				*(end + 1) = *end;
+				end--;
+			}
+			*pos = x;
+			++_finish;
+		}
+
+		iterator erase(iterator pos)  //返回所删除元素的下一个元素的位置
+		{
+			assert(pos >= _start && pos <= _finish);
+
+			iterator begin = pos + 1;
+			while (begin < _finish)
+			{
+				*(begin - 1) = *begin;
+				++begin;
+			}
+			--_finish;
+			return pos;
+		}
 
 		size_t size() const
 		{
@@ -117,43 +196,72 @@ namespace lrf
 		iterator _finish;
 		iterator _endofstorage;
 	};
-	class Solution {
-	public:
-		vector<vector<int>> generate(int numRows) {
-			vector<vector<int>> vv;
-			vv.resize(numRows);
-			for (int i = 0; i < numRows; i++)
-			{
-				vv[i].resize(i + 1);
-				vv[i][0] = vv[i][vv[i].size() - 1] = 1;
-			}
-			for (int i = 0; i < vv.size(); i++)
-			{
-				for (int j = 0; j < vv[i].size(); j++)
-				{
-					if (vv[i][j] == 0)
-						vv[i][j] = vv[i - 1][j - 1] + vv[i - 1][j];
-				}
-			}
-
-			return vv;
-		}
-	};
+	
 }
 
 void test_vector()
 {
-	lrf::vector<lrf::vector<int>> vv = lrf::Solution().generate(5);
-	for (size_t i = 0; i < vv.size(); i++)
+	lrf::vector<int> v1;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v1.push_back(4);
+	for (auto e : v1)
 	{
-		for (int j = 0; j < vv[i].size(); j++)
-		{
-			std::cout << vv[i][j] << ' ';
-		}
-		std::cout << std::endl;
+		cout << e << " ";
 	}
-	std::cout << std::endl;
+	cout << endl;
+
+
+	// 1 2 3 4 5 正常
+	// 1 2 3 4   崩溃
+	// 1 2 4 5   结果不对
+	//lrf::vector<int>::iterator it = find(v1.begin(), v1.end(), 2);	
+	lrf::vector<int>::iterator it = v1.begin();
+	while (it != v1.end())
+	{
+		if (*it % 2 == 0)
+		{
+			v1.erase(it);
+		}
+		else
+		{
+			++it;
+		}
+	}
+	for (auto e : v1)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+	//
+	//lrf::vector<int>::iterator pos = find(v1.begin(),v1.end(),2);
+	//if (pos != v1.end())
+	//{
+	//	//insert中发生了扩容，那么会导致it指向的空间被释放，
+	//	//it本质就是一个野指针，这种问题我们就叫做迭代器失效
+	//	v1.insert(v1.begin(), 20);
+	//}
+	//for (auto e : v1)
+	//{
+	//	cout << e << " ";
+	//}
+	//cout << endl;
 }
+
+//void test_vector()
+//{
+//	lrf::vector<lrf::vector<int>> vv = lrf::Solution().generate(5);
+//	for (size_t i = 0; i < vv.size(); i++)
+//	{
+//		for (int j = 0; j < vv[i].size(); j++)
+//		{
+//			std::cout << vv[i][j] << ' ';
+//		}
+//		std::cout << std::endl;
+//	}
+//	std::cout << std::endl;
+//}
 
 //void test_vector()
 //{
