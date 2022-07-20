c45ad88668ec9749b7ef1ed98945cc16a9bbb988
lrf <465197183@qq.com>
2022-07-20 22:42:12 +0800

    priority_queue模拟实现

---
+++ priority_queue/priority_queue/Test.cpp
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<queue>
+#include<functional>
+#include"priority_queue.h"
+
+using namespace std;
+
+void test_priority_queue()
+{
+	priority_queue<int,vector<int>,greater<int>> pq;  //默认大堆
+	pq.push(3);
+	pq.push(3);
+	pq.push(7);
+	pq.push(1);
+	pq.push(9);
+
+	while (!pq.empty())
+	{
+		cout << pq.top() << " ";
+		pq.pop();
+	}
+
+	cout << endl;
+}
+
+template <class T>
+struct Less
+{
+	bool operator()(const T& x, const T& y)
+	{
+		return x < y;
+	}
+};
+
+struct Greater
+{
+	bool operator()(int x, int y)
+	{
+		return x > y;
+	}
+};
+
+int main()
+{
+	//Less->仿函数 less->函数对象
+	//Less less;
+	//cout << less(1, 2) << endl;
+
+	//Greater greater;
+	//cout << greater(1, 2) << endl;
+	////lrf::test_priority_queue();
+	Less<int> less;
+	cout << less(1, 2) << endl;
+	
+	cout << Less<int>()(1, 2) << endl;
+
+	cout << Less<double>()(1, 2) << endl;
+	return 0;
+}
