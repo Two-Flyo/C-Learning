395c156ae4e469ddf668f6a68651702c6f2dbe7d
lrf <465197183@qq.com>
2022-10-25 16:09:52 +0800

    虚继承及其原理

--- Inheritance/Inheritance/Test.cpp
+++ Inheritance/Inheritance/Test.cpp
@@ -2,74 +2,156 @@
 
 using namespace std;
 
+
+
+
 class A
 {
 public:
-	A()
-	{
-		cout << "A()" << endl;
-	}
-	~A()
-	{
-		cout << "~A()" << endl;
-	}
+	int _a;
 };
-class Person
+
+class B : virtual public A
 {
-protected:
-	int _age;
-	string _name;
 public:
-	Person()
-	{
-		cout << "Person()" << endl;
-	}
-	~Person()
-	{
-		cout << "~Person()" << endl;
-	}
+	int _b;
 };
 
-class Students :public Person
+class C : virtual public A
 {
-protected:
-	int _grade;
 public:
-	Students()
-	{
-
-	}
-	Students(const Students& s)
-		:Person(s),_grade(s._grade)
-	{
-
-	}
-
-	Students& operator=(const Students& s)
-	{
-		if (this != &s)
-		{
-			Person::operator=(s);
-			_grade = s._grade;
-		}
-
-		return *this;
-	}
-	//析构函数名字会被统一处理成destructor()
-	//那么子类的析构函数跟父类的析构函数就会构成隐藏
-	//学习了多态我们才会理解为什么析构函数名会被统一处理
-	~Students()
-	{
-		Person::~Person();
-	}
+	int _c;
 };
 
+class D :public B, public C
+{
+public:
+	int _d;
+};
 
 int main()
 {
-	Students s;
+	D d;
+	d.B::_a = 1;
+	d.C::_a = 2;
+	d._b = 3;
+	d._c = 4;
+	d._d = 5;
+	d._a = 0;
 	return 0;
 }
+//class Person
+//{
+//public:
+//	string _name;
+//
+//};
+//
+//class Students : virtual public Person
+//{
+//public:
+//	int _num;
+//};
+//
+//class Teacher : virtual public Person
+//{
+//public:
+//	int _id;
+//};
+//
+//class Assistant :public Students, public Teacher
+//{
+//public:
+//	string _majorCourse;
+//};
+//
+//void Test()
+//{
+//
+//}
+//int main()
+//{
+//	Assistant a;
+//	a._id=1;
+//	a._num = 2;
+//	a.Students::_name = "lrf";
+//	a.Teacher::_name = "hhh";
+//	a._name = "abc";
+//	return 0;
+//}
+//int main()
+//{
+//
+//}
+
+//class A
+//{
+//public:
+//	A()
+//	{
+//		cout << "A()" << endl;
+//	}
+//	~A()
+//	{
+//		cout << "~A()" << endl;
+//	}
+//};
+//class Person
+//{
+//protected:
+//	int _age;
+//	string _name;
+//public:
+//	Person()
+//	{
+//		cout << "Person()" << endl;
+//	}
+//	~Person()
+//	{
+//		cout << "~Person()" << endl;
+//	}
+//};
+//
+//class Students :public Person
+//{
+//protected:
+//	int _grade;
+//public:
+//	Students()
+//	{
+//
+//	}
+//	Students(const Students& s)
+//		:Person(s),_grade(s._grade)
+//	{
+//
+//	}
+//
+//	Students& operator=(const Students& s)
+//	{
+//		if (this != &s)
+//		{
+//			Person::operator=(s);
+//			_grade = s._grade;
+//		}
+//
+//		return *this;
+//	}
+//	//析构函数名字会被统一处理成destructor()
+//	//那么子类的析构函数跟父类的析构函数就会构成隐藏
+//	//学习了多态我们才会理解为什么析构函数名会被统一处理
+//	~Students()
+//	{
+//		Person::~Person();
+//	}
+//};
+//
+//
+//int main()
+//{
+//	Students s;
+//	return 0;
+//}
 
 
 //1.A和B的fun构成函数重载
